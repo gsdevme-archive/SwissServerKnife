fec1afd67553d0ccaf23f7cf51c4a6dbdc3d5190
Gavin Staniforth <gavshouse90@gmail.com>
2012-05-11 23:33:08 +0100

    Added a protected method for rending

--- SSK/Controllers/Controller.php
+++ SSK/Controllers/Controller.php
@@ -2,12 +2,40 @@
 
 	namespace SSK\Controllers{
 
+		use \SSK\SwissServerKnife;
+		use \SSK\System\ViewRender;
+
 		abstract class Controller
 		{
 
+			protected $url, $css, $js, $img;
+
 			public function __construct()
 			{
-				
+				$this->url = (((isset($_SERVER['HTTPS'])) && (!empty($_SERVER['HTTPS']))) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/';
+
+				// Check if we have any assets, if not load from the CDN
+				if(is_dir(SwissServerKnife::getRoot() . '/assets')){
+					$this->css = $this->url . 'assets/css/';
+					$this->js = $this->url . 'assets/js/';
+					$this->img = $this->url . 'assets/img/';
+				}else{
+					die('no CDN');
+				}
+
+				$this->url .= $_SERVER['SCRIPT_NAME'] . '/';
+			}
+
+			protected function render($view, array $args=null)
+			{
+				$args = array_merge(array(
+					'url' => $this->url,
+					'css' => $this->css,
+					'js' => $this->js,
+					'img' => $this->img,
+				), (array)$args);
+
+				return ViewRender::render($view, $args);
 			}
 		}
 	}
