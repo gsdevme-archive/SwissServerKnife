1c7f658928599838201e4d97400eeecfb33d7edf
Gavin Staniforth <gavshouse90@gmail.com>
2012-05-11 23:33:16 +0100

    Removed all debug code

--- SSK/Controllers/Home.php
+++ SSK/Controllers/Home.php
@@ -2,24 +2,16 @@
 
 	namespace SSK\Controllers{
 
-		use \SSK\System\ViewRender;
-
 		class Home extends Controller
 		{
 
 			public function index()
 			{
 				$a = array(
-					'pagetitle' => 'hello',
-					'piss' => array(
-						array('title' => 'An amazing title', 'body' => 'some body text and stuff'),
-						array('title' => 'Another title', 'body' => 'foobar sticks'),
-					),
-
-					'd' => range(1,3),
+					'title' => 'hello',
 				);
 
-				ViewRender::render('test', $a);				
+				$this->render('index', $a);				
 			}
 		}
 	}
